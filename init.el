61d2988b3a796f5f11f791a4e11ff98c5879bbd6
Jean-Baptiste Potonnier <jean-baptiste.potonnier@gmail.com>
2017-01-19 22:05:38 +0100

    update config

--- init.el
+++ init.el
@@ -8,9 +8,18 @@
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))))
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(erc-modules
+   (quote
+    (autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring smiley stamp track))))
 
 (require 'org)
 (org-babel-load-file
  (expand-file-name (concat user-emacs-directory "init/init.org")))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
